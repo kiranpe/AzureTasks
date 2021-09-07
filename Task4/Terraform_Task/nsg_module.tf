c9ef1e0e58515836abb4fddcf33d9ab8a22b7a23
kiranpe <kiranwls2011@gmail.com>
2021-09-07 10:35:04 +0530

    Task 4 - Create RG, NSG, VNet and VM using Terraform Modules and Local Module

--- Task4/Terraform_Task/nsg_module.tf
+++ Task4/Terraform_Task/nsg_module.tf
@@ -1,46 +1,20 @@
 module "network-security-group" {
-  source                = "Azure/network-security-group/azurerm"
-  resource_group_name   = data.azurerm_resource_group.rg.name
-  security_group_name   = "nsg"
-  source_address_prefix = ["10.0.3.0/24"]
-  predefined_rules = [
-    {
-      name     = "SSH"
-      priority = "500"
-    },
-    {
-      name              = "LDAP"
-      source_port_range = "1024-1026"
-    }
-  ]
-
+  source              = "Azure/network-security-group/azurerm//modules/HTTP"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  security_group_name = "nsg"
   custom_rules = [
     {
-      name                   = "myssh"
-      priority               = 201
+      name                   = "ssh"
+      priority               = "100"
       direction              = "Inbound"
       access                 = "Allow"
       protocol               = "tcp"
-      source_port_range      = "*"
       destination_port_range = "22"
-      source_address_prefix  = "10.151.0.0/24"
-      description            = "description-myssh"
-    },
-    {
-      name                    = "myhttp"
-      priority                = 200
-      direction               = "Inbound"
-      access                  = "Allow"
-      protocol                = "tcp"
-      source_port_range       = "*"
-      destination_port_range  = "8080"
-      source_address_prefixes = ["10.151.0.0/24", "10.151.1.0/24"]
-      description             = "description-http"
-    },
+      source_address_prefix  = "VirtualNetwork"
+      description            = "ssh-for-vm-management"
+    }
   ]
-
   tags = {
     environment = "dev"
-    costcenter  = "it"
   }
 }
